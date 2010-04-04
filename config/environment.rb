f71c77509f29843ca6383b0c68d0ff7ff67c4a18
Franco Catena <francocatena@gmail.com>
2010-04-04 19:25:59 -0300

    Cambios estéticos y de usabilidad

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem 'validates_timeliness'
   config.gem 'i18n_label'
   config.gem 'will_paginate'
+  config.gem 'RedCloth'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
