78b3d40fbdd620e9a053875d8d5b3428a35cd289
Martín <martin.vega7@gmail.com>
2011-10-13 19:13:18 -0300

    Autocompletado de cuestiones en unidades didácticas (refs #184)

--- app/models/teaching_unit.rb
+++ app/models/teaching_unit.rb
@@ -1,6 +1,12 @@
 class TeachingUnit < ActiveRecord::Base
+  attr_accessor :nested_question
+  
   has_and_belongs_to_many :questions
   belongs_to :subtopic
   
   validates :title, :presence => true
+  
+  def initialize(attributes = nil, options = {})
+    super(attributes, options)
+  end
 end
