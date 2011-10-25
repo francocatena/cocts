0f57834ea593a3f98501422cb3c1726e1340e890
Martín Vega <martin.vega7@gmail.com>
2011-10-25 09:30:46 -0300

    Validaciones en modelos (refs #184)

--- app/models/subtopic.rb
+++ app/models/subtopic.rb
@@ -8,6 +8,7 @@ class Subtopic < ActiveRecord::Base
   
   
   validates :title, :code, :presence => true
+  validates_uniqueness_of :title 
   validates_numericality_of :code, :only_integer => true, :allow_nil => true,
     :allow_blank => true
   
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,7 @@ class Topic < ActiveRecord::Base
   has_many :subtopics, :dependent => :destroy
   
   validates :title, :code, :presence => true
+  validates_uniqueness_of :title, :code
   validates_numericality_of :code, :only_integer => true, :allow_nil => true,
     :allow_blank => true
   
