eb6e97c944408ba29355e289d96423c09275cc2e
Martín <martin.vega7@gmail.com>
2011-12-28 17:07:50 -0300

    Mejoras en tests funcionales en mailer de usuarios

--- test/functional/user_mailer_test.rb
+++ test/functional/user_mailer_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class UserMailerTest < ActionMailer::TestCase
   test 'New user notification' do
     user = users(:admin)
-    mail = UserMailer.new_user_notification(user)
+    mail = UserMailer.new_user_notification(user, 12345)
     assert_equal 'Alta de usuario en COCTS', mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ['notificaciones@cocts.com.ar'], mail.from
+    assert_equal ['soporte@cocts.com.ar'], mail.from
     assert_match "#{user.name}", mail.body.encoded
  
     assert_difference 'ActionMailer::Base.deliveries.size' do
