80484f19247cc7d70226c33f240a4860e0dc4d3d
Martín <martin.vega7@gmail.com>
2012-02-16 16:21:54 -0300

    Modificación en restricción en orden de respuestas

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -7,7 +7,7 @@ class Answer < ApplicationModel
   }
 
   # Restricciones
-  validates :answer, :category, :order, :presence => true
+  validates :answer, :category, :presence => true
   validates_numericality_of :order, :category, :only_integer => true,
     :allow_nil => true, :allow_blank => true
   validates_inclusion_of :category, :in => CATEGORIES.values,
