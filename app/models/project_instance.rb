a5fbe071afa543fde0e54ad19d8a063ac3ae3164
Martín <martin.vega7@gmail.com>
2012-04-24 19:24:29 -0300

    Correcciones en pdf de instancias de proyectos

--- app/models/project_instance.rb
+++ app/models/project_instance.rb
@@ -181,8 +181,12 @@ class ProjectInstance < ApplicationModel
       pdf.text year +": "+ self.year.to_s
     end
     pdf.text valid_until +": "+ self.valid_until.to_formatted_s(:db)
-    pdf.text group_name +": "+ self.group_name
-    pdf.text group_type +": "+ self.group_type
+    if self.group_name
+      pdf.text group_name +": "+ self.group_name
+    end
+    if self.group_type
+      pdf.text group_type +": "+ self.group_type
+    end
         
           
     # Datos sociodemográficos
