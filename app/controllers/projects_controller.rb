4fea7db3ed8fb9f3571d362ca953aea575b7965f
Martín Vega <martin.vega7@gmail.com>
2012-07-04 11:57:06 -0300

    Cambio carga de proyectos a partir de proyectos existentes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class ProjectsController < ApplicationController
   before_filter :auth
   layout proc { |controller| controller.request.xhr? ? false : 'application' }
@@ -6,20 +7,20 @@ class ProjectsController < ApplicationController
   # * GET /projects.xml
   def index
     @title = t :'projects.index_title'
-    
+
     if @auth_user.private
       @projects = Project.search(params[:search]).order('name').where('user_id = ?', @auth_user.id).paginate(
         :page => params[:page], :per_page => APP_LINES_PER_PAGE)
-      
+
     elsif @auth_user.admin
-      @projects = Project.search(params[:search]).paginate(:page => params[:page], 
+      @projects = Project.search(params[:search]).paginate(:page => params[:page],
         :per_page => APP_LINES_PER_PAGE
       )
-    
+
     else
       @projects = Project.search(params[:search]).joins(:user).where(
         "#{User.table_name}.private" => false
-      ).paginate(:page => params[:page], :per_page => APP_LINES_PER_PAGE)  
+      ).paginate(:page => params[:page], :per_page => APP_LINES_PER_PAGE)
     end
     respond_to do |format|
       format.html # index.html.erb
@@ -49,20 +50,20 @@ class ProjectsController < ApplicationController
     @title = t :'projects.new_title'
     @project = Project.new
 
-    if params[:parent_project_id].present?
-      @old = true
-      project = Project.find params[:parent_project_id]
-      @group = project.group_type
-      @test = project.test_type
+    if params[:project_name].present?
+      @name = true
+      project = Project.find_by_name params[:project_name]
       @project.name = project.name
       @project.description = project.description
+      @type = project.group_type
+      @test = project.test_type
       @project.valid_until = project.valid_until
       @project.year = project.year
       @project.forms = project.forms
       @project.teaching_units = project.teaching_units
       @project.questions = project.questions
     end
-    
+
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @project }
@@ -83,25 +84,25 @@ class ProjectsController < ApplicationController
     params[:project][:teaching_unit_ids] ||= []
     @project = Project.new(params[:project])
     @project.user = @auth_user unless @auth_user.admin
-    
+
     if @project.questions.empty? && @project.teaching_units.empty?
-      @project.errors[:base] << t(:'projects.empty_questions_error') 
+      @project.errors[:base] << t(:'projects.empty_questions_error')
       render :action => :new
     elsif !@project.teaching_units.empty? && !@project.questions.empty?
-      @project.errors[:base] << t(:'projects.questions_error') 
+      @project.errors[:base] << t(:'projects.questions_error')
       render :action => :new
     # Verifico que no sea del mismo tipo de test y tipo de grupo que el padre
-    elsif params[:project][:test_type] == params[:parent_test_type] && 
+    elsif params[:project][:test_type] == params[:parent_test_type] &&
       params[:project][:group_type] == params[:parent_group_type]
-        @project.errors[:base] << t(:'projects.type_error') 
+        @project.errors[:base] << t(:'projects.type_error')
         render :action => :new
-      
-    
+
+
     else respond_to do |format|
       @project.transaction do
         if @project.save
           @project.update_attribute :identifier, @project.generate_identifier
-          
+
           flash[:notice] = t :'projects.correctly_created'
           format.html { redirect_to projects_path }
           format.xml  { render :xml => @project, :status => :created, :location => @project }
@@ -112,32 +113,32 @@ class ProjectsController < ApplicationController
       end
      end
     end
-    
+
   end
 
   # * PUT /projects/1
   # * PUT /projects/1.xml
   def update
-    
+
     @title = t :'projects.edit_title'
-        
+
     @project = Project.find_by_identifier(params[:id])
     params[:project][:question_ids] ||= []
     params[:project][:teaching_unit_ids] ||= []
-        
+
     # Validación de que solo tenga cuestiones ya sean de UDs O cuestiones individuales
-    if params[:project][:question_ids].empty? && params[:project][:teaching_unit_ids].empty? 
-      @project.errors[:base] << t(:'projects.empty_questions_error') 
-      render :action => :edit  
+    if params[:project][:question_ids].empty? && params[:project][:teaching_unit_ids].empty?
+      @project.errors[:base] << t(:'projects.empty_questions_error')
+      render :action => :edit
     # Validación de que no tenga cuestiones de UDs Y cuestiones individuales
     elsif !(params[:project][:question_ids].blank? || @project.questions.empty?) && !(params[:project][:teaching_unit_ids].blank? || @project.teaching_units.empty?)
-      @project.errors[:base] << t(:'projects.questions_error') 
+      @project.errors[:base] << t(:'projects.questions_error')
       render :action => :edit
-    
+
     else respond_to do |format|
       @project.user = @auth_user unless @auth_user.admin
       params[:project][:identifier] = @project.generate_identifier
-      
+
       if @project.update_attributes(params[:project])
         flash[:notice] = t :'projects.correctly_updated'
         format.html { redirect_to projects_path }
@@ -166,8 +167,8 @@ class ProjectsController < ApplicationController
       format.xml  { head :ok }
     end
   end
-  
-  def pdf_rates 
+
+  def pdf_rates
     @project = Project.find_by_identifier(params[:id])
     projects = Project.where('name = ?', @project.name)
     respond_to do |format|
@@ -185,10 +186,10 @@ class ProjectsController < ApplicationController
       @projects = Project.select('distinct name')
     else
       @projects = Project.joins(:user).select('distinct projects.name').where(
-        "#{User.table_name}.private" => false)    
+        "#{User.table_name}.private" => false)
     end
   end
-    
+
   # POST /projects/auto_complete_for_question
   def autocomplete_for_question
     query = params[:q].sanitized_for_text_query
@@ -196,12 +197,12 @@ class ProjectsController < ApplicationController
     @questions = Question.scoped
     @questions = @questions.full_text(@query_terms) unless @query_terms.empty?
     @questions = @questions.limit(10)
-        
+
     respond_to do |format|
       format.json { render :json => @questions }
     end
   end
-  
+
   def preview_form
     render :partial => "#{params[:form]}"
   end
