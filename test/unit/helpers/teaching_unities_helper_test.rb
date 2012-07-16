d15bf949176fe90077046575f68c8af2c77395d5
Martín Vega <martin.vega7@gmail.com>
2012-07-16 11:38:18 -0300

    Eliminación de Temas

--- test/unit/helpers/teaching_unities_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TeachingUnitiesHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/unidad_didacticas_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class UnidadDidacticasHelperTest < ActionView::TestCase
-end
