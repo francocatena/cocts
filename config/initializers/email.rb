2e95204e148e8e464f1894c34d8d7084dea23bce
Martín Vega <martin.vega7@gmail.com>
2012-11-07 09:45:13 -0300

    Cambio dominio emails

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,13 +1,15 @@
 CoctsApp::Application.config.action_mailer.default_url_options = {
   :host => URL_HOST
 }
-CoctsApp::Application.config.action_mailer.raise_delivery_errors = true
+CoctsApp::Application.config.action_mailer.raise_delivery_errors =
+  !Rails.env.production?
 CoctsApp::Application.config.action_mailer.delivery_method = :smtp
 CoctsApp::Application.config.action_mailer.smtp_settings = {
-  :address => 'mawida.com.ar',
-  :domain => 'mawida.com.ar',
-  :port => 25,
-  :user_name => 'soporte@cocts.com.ar',
+  :address => 'smtp.gmail.com',
+  :domain => 'mawidabp.com',
+  :port => 587,
+  :user_name => 'soporte@mawidabp.com',
   :password => APP_CONFIG['smtp_password'],
-  :authentication => :plain
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }
