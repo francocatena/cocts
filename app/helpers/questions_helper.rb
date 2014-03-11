d6423d59cfec92dba33b85f2d3942ca8fe218d2c
Martín Vega <martin.vega7@gmail.com>
2014-03-11 13:11:30 -0300

    Delete questions helper

--- app/helpers/questions_helper.rb
+++
@@ -1,9 +0,0 @@
-module QuestionsHelper
-  def answer_category_field(form)
-    options = Answer::CATEGORIES.map do |k, v|
-      [t("questions.answers.long_type.#{k}"), v]
-    end
-
-    form.select :category, sort_options_array(options), {:prompt => true}
-  end
-end
