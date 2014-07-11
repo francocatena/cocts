3fc61b54e73d97d308b11fcae7cb60fe89d261f8
Martín Vega <martin.vega7@gmail.com>
2014-07-11 14:38:36 -0300

    Project instance validations module

--- app/models/project_instance.rb
+++ app/models/project_instance.rb
@@ -1,23 +1,13 @@
 class ProjectInstance < ApplicationModel
   include Reports::PdfProject
+  include ProjectInstances::Validations
+  include ProjectInstances::Scopes
+  include ProjectInstances::Relations
 
   serialize :forms, Array
 
   # Atributos no persistentes
   attr_accessor :manual_degree, :manual_degree_university
-
-  # Restricciones
-  validates :first_name, :presence => true
-  validates_numericality_of :age, :only_integer => true, :allow_nil => true,
-    :allow_blank => true
-  validates_uniqueness_of :first_name, :scope => :project_id, :allow_nil => true, :allow_blank => true
-  validates_length_of :professor_name, :first_name, :maximum => 255, :allow_nil => true,
-    :allow_blank => true
-  validates_each :forms do |record, attr, value|
-    unless (value || []).all? { |value| SOCIODEMOGRAPHIC_FORMS.include?(value) }
-      record.errors.add attr, :inclusion
-    end
-  end
 
   def initialize(attributes = nil, options = {})
     super(attributes, options)
