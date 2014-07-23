cd69d734f16155fd8d16aa9f71c32129e66139ec
Martín Vega <martin.vega7@gmail.com>
2014-07-23 11:13:01 -0300

    Subtopics respond refactor

--- app/controllers/subtopics_controller.rb
+++ app/controllers/subtopics_controller.rb
@@ -1,36 +1,21 @@
 class SubtopicsController < ApplicationController
+  respond_to :html
+
   before_filter :auth
+
   # GET /subtopics
-  # GET /subtopics.json
   def index
     @subtopics = Subtopic.search(params[:search], params[:page])
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @subtopics }
-    end
   end
 
   # GET /subtopics/1
-  # GET /subtopics/1.json
   def show
     @subtopic = Subtopic.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @subtopic }
-    end
   end
 
   # GET /subtopics/new
-  # GET /subtopics/new.json
   def new
     @subtopic = Subtopic.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @subtopic }
-    end
   end
 
   # GET /subtopics/1/edit
@@ -39,23 +24,13 @@ class SubtopicsController < ApplicationController
   end
 
   # POST /subtopics
-  # POST /subtopics.json
   def create
     @subtopic = Subtopic.new(subtopic_params)
-
-    respond_to do |format|
-      if @subtopic.save
-        format.html { redirect_to subtopics_path, notice: t('subtopics.correctly_created') }
-        format.json { render json: @subtopic, status: :created, location: @subtopic }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @subtopic.errors, status: :unprocessable_entity }
-      end
-    end
+    @subtopic.save
+    respond_with @subtopic, location: subtopics_url
   end
 
   # PUT /subtopics/1
-  # PUT /subtopics/1.json
   def update
     @subtopic = Subtopic.find(params[:id])
     params[:subtopic][:teaching_unit_ids] ||= []
@@ -72,15 +47,10 @@ class SubtopicsController < ApplicationController
   end
 
   # DELETE /subtopics/1
-  # DELETE /subtopics/1.json
   def destroy
     @subtopic = Subtopic.find(params[:id])
     @subtopic.destroy
-
-    respond_to do |format|
-      format.html { redirect_to subtopics_url }
-      format.json { head :ok }
-    end
+    respond_with @subtopic
   end
 
   def autocomplete_for_teaching_unit
@@ -91,7 +61,7 @@ class SubtopicsController < ApplicationController
     @teaching_units = @teaching_units.limit(10)
 
     respond_to do |format|
-      format.json { render :json => @teaching_units }
+      format.json { render json: @teaching_units }
     end
   end
 
