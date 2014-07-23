dcae3176d704e27762bb92b6cbe312f4b6b7e4d1
Martín Vega <martin.vega7@gmail.com>
2014-07-23 10:38:40 -0300

    Import csv module

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  require 'csv'
+  include ImportCsv
 
   respond_to :html
 
@@ -57,84 +57,6 @@ class QuestionsController < ApplicationController
     unless @auth_user.admin?
       flash[:alert] = t :'users.admin_error'
       redirect_to questions_path
-    end
-  end
-
-  def csv_import_questions
-    if params[:dump_questions] && File.extname(params[:dump_questions][:file].original_filename).downcase == '.csv'
-      uploaded_file = params[:dump_questions][:file]
-      file_name = uploaded_file.path.to_s
-      text = File.read(
-        file_name,
-        { :encoding => 'UTF-8',
-          :delimiter => ';'
-        }
-      )
-
-      @parsed_file=CSV.parse(text, :col_sep => ';')
-      n=0
-      @parsed_file.each  do |row|
-        q = Question.new
-        q.dimension = row[0].to_i
-        q.code = row[1]
-        q.question = row[2]
-        if q.save
-          n+=1
-        end
-      end
-      flash[:notice] = t(:'questions.csv_import', :count => n)
-    else
-      flash[:alert] = t :'questions.error_file_extension'
-    end
-    respond_to do |format|
-      format.html { redirect_to(questions_path) }
-    end
-  end
-
-  def csv_import_answers
-    if params[:dump_answers] && File.extname(params[:dump_answers][:file].original_filename).downcase == '.csv'
-      uploaded_file = params[:dump_answers][:file]
-      file_name = uploaded_file.path.to_s
-      text = File.read(
-        file_name,
-        { :encoding => 'UTF-8',
-          :delimiter => ';'
-        }
-      )
-
-      @parsed_file=CSV.parse(text, :col_sep => ';')
-      n=0
-      @parsed_file.each  do |row|
-        a = Answer.new
-        category = row[1]
-        if category == 'A'
-          category = 2
-        elsif category == 'P'
-          category = 1
-        elsif category == 'I'
-          category = 0
-        end
-        a.category = category
-        a.order = row[2].to_i
-        a.clarification = row[4]
-        a.answer = row[5]
-        question = Question.find_by_code(row[0])
-        unless question.blank?
-          a.question_id = question.id
-        end
-        if a.save
-          n+=1
-        end
-        unless question.blank?
-          question.answers << a
-        end
-      end
-      flash[:notice] = t(:'questions.answers.csv_import', :count => n)
-    else
-      flash[:alert] = t :'questions.error_file_extension'
-    end
-    respond_to do |format|
-      format.html { redirect_to(questions_path) }
     end
   end
 
