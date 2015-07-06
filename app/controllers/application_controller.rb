0fdd97c08cc3cc95e1bf65d7af71aa2e2aed9c2a
Martín Vega <martin.vega7@gmail.com>
2015-07-06 09:44:56 -0300

    Remove generic exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,23 +11,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, alert: exception.message
   end
 
-  # Cualquier excepción no contemplada es capturada por esta función. Se utiliza
-  # para mostrar un mensaje de error personalizado
-  rescue_from Exception do |exception|
-    begin
-      @title = t 'errors.title'
-
-      unless response.redirect_url
-        render template: 'errors/show', locals: { error: exception }
-      end
-
-    # En caso que la presentación misma de la excepción no salga como se espera
-    rescue => ex
-      STDERR << "#{ex.class}: #{ex.message}\n\n"
-      ex.backtrace.each { |l| STDERR << "#{l}\n" }
-    end
-  end
-
   private
 
     # Verifica que el login se haya realizado y el usuario esté activo
